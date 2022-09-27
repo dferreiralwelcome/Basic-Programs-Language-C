7cff7db6673bd1712e4cb81670728eb2b5bc2ee4
Daniel Ferreira <114479483+DanielFerreira96@users.noreply.github.com>
2022-09-27 10:45:18 +0100

    Add files via upload

---
+++ Função c.retorno.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void minhafuncao(){
+	
+	printf("Media entre dois numeros:\n");
+}
+	
+int media(n1,n2,n3){
+	return (n1 + n2) / n3;
+}
+
+
+int main(){
+	
+	minhafuncao();
+	
+	int resultado;
+	resultado = media(2,12,2);
+	printf("\nresultado = %d\n", resultado);
+	
+	
+
+return 0;
+}
